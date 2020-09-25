aff4d963559db655f596b8adf48e76cec8f933d2
Ryan Makund <ryanmakund@gmail.com>
2020-09-24 22:20:40 -0400

    added operator-to CartesianPoint class

--- OOP3200-F2020-Lesson2/CartesianPoint.h
+++ OOP3200-F2020-Lesson2/CartesianPoint.h
@@ -12,15 +12,19 @@ public:
 	/* Constructor: Used to initialize objects */
 	
 	CartesianPoint(int x = 1, int y = 1);
+	~CartesianPoint();
 
+	/*operator overloads */
+	double operator-(const CartesianPoint& point_to) const;
+	
 	// --------------------------------------------------------------------------------
 	/* Accessors: Used to query the state of the object */
 	
 	// get x
-	int GetX();
+	int GetX() const;
 
 	// get y
-	int GetY();
+	int GetY() const;
 
 	// -------------------------------------------------------------------------------
 	/* Mutator(s): Used to change the state of the object
@@ -39,7 +43,7 @@ public:
 
 
 	// get the distance between this point and a second point
-	double GetDistanceTo(CartesianPoint pointTo) const;
+	double GetDistanceTo(const CartesianPoint& point_to) const;
 
 	// convert the obj to a string
 	std::string ToString() const;
@@ -47,8 +51,8 @@ public:
 
 private:
 	// private data members for the dimensions of the point
-	int myX; // x-axis (horizontal) value
-	int myY;  // y-axis (vertical) value
+	int myX{}; // x-axis (horizontal) value
+	int myY{};  // y-axis (vertical) value
 
 };
 
