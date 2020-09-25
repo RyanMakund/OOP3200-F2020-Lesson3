9498d5feefd0b5eb02880890b581e5ae623278e1
Ryan Makund <ryanmakund@gmail.com>
2020-09-24 21:54:53 -0400

    restructured project

---
+++ OOP3200-F2020-Lesson2/CartesianPoint.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef _CARTESIAN_POINT_
+#define _CARTESIAN_POINT_
+
+#include <string>
+
+// class declaration section
+class CartesianPoint
+{
+public:
+
+	/* Constructor: Used to initialize objects */
+	
+	CartesianPoint(int x = 1, int y = 1);
+
+	// --------------------------------------------------------------------------------
+	/* Accessors: Used to query the state of the object */
+	
+	// get x
+	int GetX();
+
+	// get y
+	int GetY();
+
+	// -------------------------------------------------------------------------------
+	/* Mutator(s): Used to change the state of the object
+	*	- should contain logic to ensure object remains in a valid state.
+	*	- typically sets a member variable to a parameter
+	*/
+
+	// set x
+	void SetX(int x);
+
+	// set y,
+	void SetY(int y);
+
+	// set point (both x and y)
+	void SetPoint(int x, int y);
+
+
+	// get the distance between this point and a second point
+	double GetDistanceTo(CartesianPoint pointTo) const;
+
+	// convert the obj to a string
+	std::string ToString() const;
+
+
+private:
+	// private data members for the dimensions of the point
+	int myX; // x-axis (horizontal) value
+	int myY;  // y-axis (vertical) value
+
+};
+
+#endif /* defined (_CARTESIAN_POINT__)*/
+
